073996bfd401961d6c835bdabc02bf8142fe5183
Mohamed Sameh <mohamedsameh1574@gmail.com>
2024-05-23 17:27:47 +0300

    added some queries to get data
    added a few queries to read data from the libraries along side their funtionalities

--- Backend/Starting project/SRC/library/controller.js
+++ Backend/Starting project/SRC/library/controller.js
@@ -4,18 +4,49 @@ const queries = require('./queries');
 const getLibraries = (req, res) => {
     pool.query(queries.getLibraries, (error, results) => {
         if (error) throw error;
-        res.status(200).json(results.rows)
+        res.status(200).json(results.rows);
     })
 };
 
 const getLibrariesById = (req, res) => {
-    pool.query(queries.getLibrariesById, (error, results) => {
-        if (error) throw error
-        res.status(200).json(results.rows)
-    })
+    const libraryID = parseInt(req.params.id);
+    pool.query(queries.getLibrariesById, [libraryID], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    } )
 };
+
+const getNoBooksFromLibrary = (req, res) => {
+    const libraryID = parseInt(req.params.id);
+    pool.query(queries.getNoBooksFromLibrary, [libraryID], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    } )
+}
+
+const getBookDataByISBN = (req, res) => {
+    const ISBN_Entry = req.params.ISBN;
+    pool.query(queries.getNoBooksFromLibrary, [ISBN_Entry], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    })
+}
+
+const getBookAuthors = (req, res) => {
+    const ISBN_Entry = req.params.ISBN
+    pool.query(queries.getBookAuthors, [ISBN_Entry], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    })
+}
+
+
 
 module.exports = {
     getLibraries,
     getLibrariesById,
+    getNoBooksFromLibrary,
+    getBookDataByISBN,
+    getBookAuthors,    
+    
 }
