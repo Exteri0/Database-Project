66f186efd6c116f49637e26c6f8b732b19b4513e
Exteri0 <omarfaramawy19@gmail.com>
2024-05-25 14:59:48 +0300

    finished the hero section

--- Frontend/src/pages/Homepage.tsx
+++ Frontend/src/pages/Homepage.tsx
@@ -1,13 +1,36 @@
 import Navbar from '../components/Navbar'
 import '../styles/homepage.css'
+import libraryImg from '../assets/library.png'
+import bshelf from '../assets/Bookshelf Icon.png'
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
     return (
-        <div className="big-container">
+        <div className="home-container">
             <Navbar />
-            <div className="container">
-                <h1>yes it's working</h1>
+            <div className="hero-section">
+                <div className="text-cta">
+                    <div>
+                        <img className="logo" src={bshelf} />
+                        <span>BookShelf:</span>
+                    </div>
+                    <span>The World of books from</span>
+                    <span style={{ color: '#4caf4f' }}>
+                        3 different libraries
+                    </span>
+                    <Link to={'login'} className="login-cta">
+                        <span>Login</span>
+                    </Link>
+                </div>
+
+                <img className="library-img" src={libraryImg} />
             </div>
+            <div id="libraries-section">
+                <h3>Our Libraries</h3>
+                <div className="library-cards"></div>
+            </div>
+            <div id="whyus-section"></div>
+            <div className="cta"></div>
         </div>
     )
 }
